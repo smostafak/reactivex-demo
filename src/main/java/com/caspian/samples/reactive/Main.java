53c08248239ccf328cb82b5f9eb079c1a93373da
Mostafa Kalantar <mostafa.kalantar@gmail.com>
2019-06-13 19:54:02 +0430

    Add clarifying comments

--- src/main/java/com/caspian/samples/reactive/Main.java
+++ src/main/java/com/caspian/samples/reactive/Main.java
@@ -11,8 +11,8 @@ public final class Main {
   public static void main(String... args) {
     // Create reactive stream which emits only one element
     final Observable<Object> stream = Observable.create(emitter -> {
-      emitter.onNext("Hello world");
-      emitter.onComplete();
+      emitter.onNext("Hello world");  // emitter.emit("Hello world");
+      emitter.onComplete();                 // emitter.finish();
     });
 
     // Subscribe to the reactive stream and println each element as it is emitted
