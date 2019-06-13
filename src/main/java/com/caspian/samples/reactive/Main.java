3b1053d778539ff12bf9edbfbf9cfe74652f9676
Mostafa Kalantar <mostafa.kalantar@gmail.com>
2019-06-13 19:49:01 +0430

    Simple synchronous emitter

---
+++ src/main/java/com/caspian/samples/reactive/Main.java
@@ -0,0 +1,21 @@
+package com.caspian.samples.reactive;
+
+import io.reactivex.Observable;
+
+/**
+ * @author Mostafa Kalantar (kalantar@caspco.ir)
+ * @version 1.0 2019.0613
+ * @since 1.0
+ */
+public final class Main {
+  public static void main(String... args) {
+    // Create reactive stream which emits only one element
+    final Observable<Object> stream = Observable.create(emitter -> {
+      emitter.onNext("Hello world");
+      emitter.onComplete();
+    });
+
+    // Subscribe to the reactive stream and println each element as it is emitted
+    stream.subscribe(System.out::println);
+  }
+}
