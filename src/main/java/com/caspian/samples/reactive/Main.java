003a0775062591dc88f2dc73bde1ec508763e02c
Mostafa Kalantar <mostafa.kalantar@gmail.com>
2019-06-27 20:24:01 +0430

    Emits 0L after 1 sec

--- src/main/java/com/caspian/samples/reactive/Main.java
+++ src/main/java/com/caspian/samples/reactive/Main.java
@@ -2,33 +2,23 @@ package com.caspian.samples.reactive;
 
 import io.reactivex.Observable;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author Mostafa Kalantar (kalantar@caspco.ir)
  * @version 1.0 2019.0613
  * @since 1.0
  */
 public final class Main {
-  private static Integer load(int id) {
-    if (id == 1) {
-      throw new IllegalStateException();
-    }
-    
-    return id + 1;
+  private static void log(Object msg) {
+    System.out.println(Thread.currentThread().getName() + ": " + msg);
   }
 
-  static Observable<Integer> rxLoad(int id) {
-    return Observable.create(emitter -> {
-      try {
-        emitter.onNext(load(id));
-        emitter.onComplete();
-      } catch (Exception e) {
-        emitter.onError(e);
-      }
-    });
-  }
+  public static void main(String... args) throws InterruptedException {
+    Observable
+        .timer(1, TimeUnit.SECONDS)       // an asynchronous equivalent of Thread.sleep().
+        .subscribe(Main::log);
 
-  public static void main(String... args) {
-    rxLoad(2).subscribe(System.out::println);
-    rxLoad(1).subscribe(System.out::println);
+    Thread.currentThread().join(2000);
   }
 }
